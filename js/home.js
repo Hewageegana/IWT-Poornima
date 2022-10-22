0b8adee1ced8f83672a31639ed29c652e6655c65
Damru Ravihara <damru@agrithmics.lk>
2022-10-22 20:53:08 +0530

    home page header and footer

--- js/home.js
+++ js/home.js
@@ -3,8 +3,8 @@ function checkAccess() {
     var y = document.getElementById("login");
     // var dc = document.cookie;
     // let user = dc.getCookie("user");
-    let user = document.cookie.search('user')
-    console.log(user)
+
+    let user = getCookie('user')
     if(user != undefined && user != null && user.length > 0){
         y.style.display= 'display'
         x.style.display= 'none'
@@ -14,4 +14,25 @@ function checkAccess() {
         x.style.display= 'display'
     }
 
+}
+
+
+function getCookie(name) {
+    // Split cookie string and get all individual name=value pairs in an array
+    var cookieArr = document.cookie.split(";");
+    
+    // Loop through the array elements
+    for(var i = 0; i < cookieArr.length; i++) {
+        var cookiePair = cookieArr[i].split("=");
+        
+        /* Removing whitespace at the beginning of the cookie name
+        and compare it with the given string */
+        if(name == cookiePair[0].trim()) {
+            // Decode the cookie value and return
+            return decodeURIComponent(cookiePair[1]);
+        }
+    }
+    
+    // Return null if not found
+    return null;
 }
